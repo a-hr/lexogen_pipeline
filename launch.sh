921b042aa0da2446bdf1c975dfd2e96fb79ce87f
a-hr <heerreroalvaro@gmail.com>
2023-02-09 20:09:05 +0100

    update

--- launch.sh
+++ launch.sh
@@ -8,4 +8,4 @@ module load Singularity Python Nextflow
 # limit the resources of the JVM
 export NXF_OPTS="-Xms500M -Xmx2G"
 # launch the main process
-nextflow run main.nf -resume -with-trace -profile cluster -params-file input_params.yaml
+nextflow run main.nf -resume -profile cluster -params-file input_params.yaml
